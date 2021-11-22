e640fa417b865dbb63468aad392a09b5a7820922
jereyes1 <70930622+jereyes1@users.noreply.github.com>
2021-11-22 16:32:10 -0500

    Initial Commit - Function genPick and rocpkPick
    Logic is still flawed but when user clicks/picks rock it generates an answer.

---
+++ js/page.js
@@ -0,0 +1,30 @@
+function initialPick() {
+  var initial = "svg/mystery.svg";
+  document.getElementById('oppResult').src = initial;
+}
+
+function genPick() {
+
+    var pick = "svg/mystery.svg";
+    if(Math.floor(Math.random() * 3) == 2) {
+      var pick = "svg/rock.svg"
+    } else if (Math.floor(Math.random() * 3) == 1) {
+      var pick = "svg/paper.svg"
+    } else {
+      var pick = "svg/scissors.svg"
+    }
+    document.getElementById('oppResult').src = pick;
+}
+
+
+function rockPick(pick) {
+    genPick();
+    if(pick == "svg/rock.svg") {
+      document.getElementById('result').innerHTML = "It's a Tie";
+    }  else if (pick == "svg/paper.svg") {
+      document.getElementById('result').innerHTML = "You Lose";
+    } else {
+      document.getElementById('result').innerHTML= "You Win!";
+    }
+
+}
