766aae92ea66d058dd2825330d019ac74eb7c92a
jereyes1 <70930622+jereyes1@users.noreply.github.com>
2021-11-23 11:45:29 -0500

    Fixed the genPick() function and the function call to it in other functions
    The function call to genPick for rockPick(), paperPick() and scissorPick() was flawed. I returned pick in genPick() function and set pick to equal genPick in the other functions. I also fixed some logic in the if else statements

--- js/page.js
+++ js/page.js
@@ -3,8 +3,9 @@ function initialPick() {
   document.getElementById('oppResult').src = initial;
 }
 
-function genPick() {
 
+
+function genPick() {
     var pick = "svg/mystery.svg";
     if(Math.floor(Math.random() * 3) == 2) {
       var pick = "svg/rock.svg"
@@ -13,18 +14,47 @@ function genPick() {
     } else {
       var pick = "svg/scissors.svg"
     }
-    document.getElementById('oppResult').src = pick;
+    document.getElementById('oppResult').src = pick
+    return pick;
 }
 
+comp_score = 0;
+user_score = 0;
 
-function rockPick(pick) {
-    genPick();
+function rockPick() {
+    pick = genPick();
     if(pick == "svg/rock.svg") {
       document.getElementById('result').innerHTML = "It's a Tie";
     }  else if (pick == "svg/paper.svg") {
+      document.getElementById('result').innerHTML = "You Lose";
+      comp_score = comp_score + 1;
+    } else {
+      document.getElementById('result').innerHTML= "You Win!";
+      user_score = user_score + 1;
+    }
+}
+
+function paperPick() {
+    pick = genPick();
+    if(pick == "svg/paper.svg") {
+      document.getElementById('result').innerHTML = "It's a Tie";
+    }  else if (pick == "svg/scissors.svg") {
       document.getElementById('result').innerHTML = "You Lose";
     } else {
       document.getElementById('result').innerHTML= "You Win!";
     }
 
 }
+function scissorsPick() {
+    pick = genPick();
+    if(pick == "svg/scissors.svg") {
+      document.getElementById('result').innerHTML = "It's a Tie";
+    }  else if (pick == "svg/rock.svg") {
+      document.getElementById('result').innerHTML = "You Lose";
+    } else {
+      document.getElementById('result').innerHTML= "You Win!";
+    }
+}
+
+sessionStorage.setItem("",);
+sessionStorage.getItem("");
